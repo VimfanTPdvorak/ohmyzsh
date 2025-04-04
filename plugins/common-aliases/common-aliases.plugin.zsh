b41071f5d986ec48ae6540a3007e5f366814c9d0
Billy <russet.07.scuff@icloud.com>
2025-04-04 19:44:43 +0700

    Add new aliases and update file handlers
    - Added 'lat' alias for ls with almost all files sorted by date.
    - Added 'vimrc' alias for quick access to .vimrc.
    - Updated file extension handlers to use modern tools like 'kitten icat' for
      images and 'sioyek' for PDFs.
    - Improved media file handling with 'ffplay' and 'mpv'.
    - Added support for more file types in editor aliases.
    
    (cherry picked from commit 8bbf55153db8ef95d4787c78e949b78211a54947)

--- plugins/common-aliases/common-aliases.plugin.zsh
+++ plugins/common-aliases/common-aliases.plugin.zsh
@@ -1,6 +1,5 @@
 # Advanced Aliases.
 # Use with caution
-#
 
 # ls, the common ones I use a lot shortened for rapid fire usage
 alias l='ls -lFh'     #size,show type,human readable
@@ -8,6 +7,7 @@ alias la='ls -lAFh'   #long list,show almost all,show type,human readable
 alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
 alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
 alias ll='ls -l'      #long list
+alias lat='ls -lAFht'
 alias ldot='ls -ld .*'
 alias lS='ls -1FSsh'
 alias lart='ls -1Fcart'
@@ -16,6 +16,7 @@ alias lsr='ls -lARFh' #Recursive list of files and directories
 alias lsn='ls -1'     #A column contains name of files and directories
 
 alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc' # Quick access to the .zshrc file
+alias vimrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.vimrc' # Quick access to the .vimrc file
 
 alias grep='grep --color'
 alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
@@ -54,36 +55,36 @@ alias mv='mv -i'
 # depends on the SUFFIX :)
 autoload -Uz is-at-least
 if is-at-least 4.2.0; then
-  # open browser on urls
-  if [[ -n "$BROWSER" ]]; then
-    _browser_fts=(htm html de org net com at cx nl se dk)
-    for ft in $_browser_fts; do alias -s $ft='$BROWSER'; done
-  fi
+    # open browser on urls
+    if [[ -n "$BROWSER" ]]; then
+        _browser_fts=(htm html de org net com at cx nl se dk)
+        for ft in $_browser_fts; do alias -s $ft='$BROWSER'; done
+    fi
 
-  _editor_fts=(cpp cxx cc c hh h inl asc txt TXT tex)
-  for ft in $_editor_fts; do alias -s $ft='$EDITOR'; done
+    _editor_fts=(cpp cxx cc c hh h inl txt TXT tex prg spr wiki md)
+    for ft in $_editor_fts; do alias -s $ft='$EDITOR'; done
 
-  if [[ -n "$XIVIEWER" ]]; then
     _image_fts=(jpg jpeg png gif mng tiff tif xpm)
-    for ft in $_image_fts; do alias -s $ft='$XIVIEWER'; done
-  fi
+    for ft in $_image_fts; do alias -s $ft='kitten icat'; done
 
-  _media_fts=(ape avi flv m4a mkv mov mp3 mpeg mpg ogg ogm rm wav webm)
-  for ft in $_media_fts; do alias -s $ft=mplayer; done
+    _media_fts=(ape avi flv m4a mkv mov mp3 mpeg mpg ogg ogm rm wav webm)
+    for ft in $_media_fts; do alias -s $ft='ffplay -nodisp -autoexit 2> /dev/null'; done
 
-  #read documents
-  alias -s pdf=acroread
-  alias -s ps=gv
-  alias -s dvi=xdvi
-  alias -s chm=xchm
-  alias -s djvu=djview
+    #read documents
+    alias -s pdf='sioyek 2> /dev/null >&2'
+    alias -s ps=gv
+    alias -s dvi=xdvi
+    alias -s chm=xchm
+    alias -s djvu=djview
+    alias -s {asc,gpg}='gpgOpen -f'
+    alias -s mp4='mpv --vo=kitty'
 
-  #list whats inside packed file
-  alias -s zip="unzip -l"
-  alias -s rar="unrar l"
-  alias -s tar="tar tf"
-  alias -s tar.gz="echo "
-  alias -s ace="unace l"
+    #list whats inside packed file
+    alias -s zip="unzip -l"
+    alias -s rar="unrar l"
+    alias -s tar="tar tf"
+    alias -s tar.gz="echo "
+    alias -s ace="unace l"
 fi
 
 # Make zsh know about hosts already accessed by SSH
